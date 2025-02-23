65990ae961fcca19f216843fd93331eac3fb53bd
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-02-23 20:53:53 -0300

    Alterando lógica de inserção, criação e retorno do valor

--- src/example.c
+++ src/example.c
@@ -1,17 +1,44 @@
-#include <stdio.h>
 #include "hash_table.h"
+#include <ctype.h>
+#include <stdio.h>
 
 #define TAM 8
 
 int main() {
-  HashTable* hash_table = create_hash_table(TAM);
+  HashTable *hash_table = create_hash_table(TAM);
 
   if (hash_table == NULL) {
     fprintf(stderr, "Cannot create hash table");
-    return -1;
+    return 1;
   }
 
-  printf("Hash table created successfully!");
+  printf("Hash table created successfully!\n");
+
+  if (add_key_value(hash_table, "City", "Curitiba") == 0) {
+    char *value = get_value(hash_table, "City");
+    if (value != NULL) {
+      printf("%s\n", value);
+    }
+  }
+
+  // Trying to access a key that is not in the hash table
+  if (get_value(hash_table, "nonexistent") == NULL) {
+    printf("\nKey not present in the hash_table");
+  }
+
+  char input_key[50], input_value[50];
+  printf("\n\nLet's create a new entry! Provide the key: ");
+  scanf("%49s", input_key);
+
+  printf("Now a value: ");
+  scanf("%49s", input_value);
+
+  if (add_key_value(hash_table, input_key, input_value) == 0) {
+    char *value = get_value(hash_table, input_key);
+    if (value != NULL) {
+      printf("\nKey: %s\nValue: %s\n", input_key, value);
+    }
+  }
 
   return 0;
 }
--- src/hash_table.c
+++ src/hash_table.c
@@ -1,22 +1,107 @@
 #include "hash_table.h"
+#include <stdio.h>
+#include <string.h>
 
-HashTable* create_hash_table(size_t size) {
-  HashTable* hash_table = malloc(sizeof (HashTable));
+// Returns NULL in case of error, otherwise returns a pointer to dynamically
+// allocated hash table
+HashTable *create_hash_table(size_t size) {
+  HashTable *hash_table = malloc(sizeof(HashTable));
   if (hash_table == NULL) {
     return NULL;
   }
 
-  hash_table->items = malloc(sizeof (ListNode) * size);
+  hash_table->items = malloc(sizeof(ListNode *) * size);
   if (hash_table->items == NULL) {
+    free(hash_table); // In case of fail for items allocation, free the memory
+                      // in use by the hash_table
     return NULL;
   }
 
-  // Initializing each hash table head to point to nothing (NULL)
+  // Initializing each hash table head with NULL
   for (size_t i = 0; i < size; i++) {
-    hash_table->items[i].next = NULL;
+    hash_table->items[i] = NULL;
   }
 
   hash_table->size = size;
 
   return hash_table;
+}
+
+size_t djb2_hash(size_t size, char *key) {
+  unsigned long hash = 5381;
+  for (size_t i = 0; key[i] != '\0'; i++) {
+    hash = (hash << 5) + hash + key[i];
+  }
+
+  return hash % size;
+}
+
+// Returns 1 in case of erorr, else 0
+int add_key_value(HashTable *hash_table, char *key, char *value) {
+  if (hash_table == NULL || key == NULL || value == NULL) {
+    return 1;
+  }
+
+  ListNode *new_node = malloc(sizeof(ListNode));
+  if (new_node == NULL) {
+    return 1;
+  }
+
+  size_t index = djb2_hash(hash_table->size, key);
+
+  new_node->key = malloc(strlen(key) + 1);
+  if (new_node->key == NULL) {
+    free(new_node);
+    return 1;
+  }
+  strcpy(new_node->key, key); // Creating a copy of the key
+
+  new_node->value = malloc(strlen(value) + 1);
+  if (new_node->value == NULL) {
+    free(new_node->key);
+    free(new_node);
+    return 1;
+  }
+  strcpy(new_node->value, value); // Creating a copy of the value
+
+  // Occurs when there is no collision
+  if (hash_table->items[index] == NULL) {
+    new_node->next = NULL;
+    hash_table->items[index] = new_node;
+    return 0;
+  }
+
+  ListNode *current = hash_table->items[index];
+  while (current->next != NULL) {
+    if (strcmp(current->key, key) == 0) {
+      return 1;
+    }
+    current = current->next;
+  }
+
+  // Appending the new node
+  current->next = new_node;
+  new_node->next = NULL;
+
+  return 0;
+}
+
+// Return NULL if the key is not found or other error, otherwise, returns the
+// associated value
+char *get_value(HashTable *hash_table, char *key) {
+  if (hash_table == NULL || key == NULL) {
+    return NULL;
+  }
+
+  // Getting where the key is supposed to be
+  int index = djb2_hash(hash_table->size, key);
+
+  ListNode *current = hash_table->items[index];
+  while (current != NULL) {
+    if (strcmp(current->key, key) == 0) {
+      return current->value;
+    }
+    current = current->next;
+  }
+  return NULL;
 }
