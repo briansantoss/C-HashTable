52b276ca4a4f4afa226d7d6557b89671d575266a
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-02-23 20:53:20 -0300

    Removendo tipos desnecessários
    Agora a hash table contém os dois elementos do par (chave e valor) como
    strings

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,40 +1,22 @@
 #ifndef HASHTABLE_H
 
-#include <stdlib.h>
 #include <stdbool.h>
-
-typedef union {
-  char* string_val;
-  int* int_val;
-  float* float_val;
-  double* double_val;
-  bool* bool_val;
-} DictValue;
-
-typedef enum {
-  STRING_TYPE,
-  INT_TYPE,
-  FLOAT_TYPE,
-  DOUBLE_TYPE,
-  BOOL_TYPE
-} DictValueType;
-
-typedef struct {
-  char* key;
-  DictValue value;
-  DictValueType value_type;
-} DictItem;
+#include <stdlib.h>
 
 typedef struct ListNode {
-  DictItem dict;
-  struct ListNode* next;
+  char *key;
+  char *value;
+  struct ListNode *next;
 } ListNode;
 
 typedef struct {
-  ListNode* items;
+  ListNode **items;
   size_t size;
 } HashTable;
 
-HashTable* create_hash_table(size_t size);
+HashTable *create_hash_table(size_t size);
+int add_key_value(HashTable *hash_table, char *key, char *value);
+char *get_value(HashTable *hash_table, char *key);
+void free_hash_table(HashTable *hash_table);
 
 #endif
